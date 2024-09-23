bf42e9f1585b98da5026e23dd1ec7e582ac16f78
amanda <amandaleticiaraymundo@gmail.com>
2024-09-23 09:01:30 -0300

    continuou

--- showdomilhao/Gerenciador.cs
+++ showdomilhao/Gerenciador.cs
@@ -1,8 +1,13 @@
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.UI.Xaml.Automation.Peers;
 using showdomilhao;
 
 public class Gerenciador 
 {
-    void CriarQuestoes (Label labelPergunta, , Button btResp02, Button btResp03, Button btResp04, Button btResp05)
+         CriarQuestoes (Label labelPergunta, Button buttontResp01, buttontResp02, Button buttonResp03, Button buttonResp04, Button buttonResp05)
+    }
+{}
+    void CriarQuestoes (Label labelPergunta, Button buttonResp01 Button butttonResp02, Button buttonResp03, Button buttonResp04, Button buttonResp05)
     {
         var q1 = new Questao();
         q1.pergunta= "Quanto é 2+2 ?";
@@ -10,10 +15,27 @@ public class Gerenciador
         q1.resposta02="3";
         q1.resposta03="11";
         q1.resposta04="4";
+        q1.resposta05="12";
         q1.respostacorreta = 4;
-        q1.ConfigurarDesenho(labelPergunta, btnResposta01, btnResposta02, btnResposta03, btnResposta04);
-        listaQuestoes.Add(q1);
         
+        public void ProximaQuestao()
+        {
+            var numRandomico= Random.Shared.Next(0, listaQuestoes.Count);
+            while(listaQuestoesRespondidas.Countains(numRandomico))
+             numRamdomico=Random.Shared.Next(0,listaQuestoes.count);
 
-    }
+             listaQuestoesRespondidas.Add(numRandomico);
+             QuestaoCorrente= listaQuestoes[numRandomico];
+             QuestaoCorrente.Desenha();
+        }
+        
+        public async void verificaCorreto (int RR)
+        {
+            if (QuestaoCorrent.Verifica Resposta(RR))
+            {
+                await Task.Delay (1000);
+                ProximaQuestao();
+            }
+        }
+    }   
 }
